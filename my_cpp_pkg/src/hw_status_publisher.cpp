55d5de295276c38e8540ad8b0fa35fd711567390
Raiano Martins <RaianoMartins@poli.ufrj.br>
2024-01-29 23:47:52 -0300

    Section 7 complete

--- my_cpp_pkg/src/hw_status_publisher.cpp
+++ my_cpp_pkg/src/hw_status_publisher.cpp
@@ -6,8 +6,14 @@ class HardwarePublisher : public rclcpp::Node
 public:
     HardwarePublisher(): Node("hardware_status_publisher")
     {
+        this->declare_parameter("temperature_to_publish",40);
+        this->declare_parameter("publish_frequency",1.0);
+
+        temperature = this->get_parameter("temperature_to_publish").as_int();
+        double publish_frequency = this->get_parameter("publish_frequency").as_double();
+
         publisher = this->create_publisher<my_robot_interfaces::msg::HardwareStatus>("hardware_status",10);
-        timer = this->create_wall_timer(std::chrono::milliseconds(1000),
+        timer = this->create_wall_timer(std::chrono::milliseconds((int)(1000.0/publish_frequency)),
                                         std::bind(&HardwarePublisher::publishNews,this));
         RCLCPP_INFO(this->get_logger(), "Hardware status publisher iniciou!");
     }
@@ -16,12 +22,13 @@ private:
     void publishNews()
     {
         auto msg = my_robot_interfaces::msg::HardwareStatus();
-        msg.temperature = 57;
+        msg.temperature = temperature;
         msg.are_motors_ready = false;
         msg.debug_message = "Motores estão muito quentes!";
         publisher->publish(msg);
     }
 
+    int temperature;
     rclcpp::Publisher<my_robot_interfaces::msg::HardwareStatus>::SharedPtr publisher;
     rclcpp::TimerBase::SharedPtr timer;
 };
--- my_py_pkg/my_py_pkg/number_publisher.py
+++ my_py_pkg/my_py_pkg/number_publisher.py
@@ -7,9 +7,13 @@ from example_interfaces.msg import Int64
 class NumberPublisherNode(Node):
     def __init__(self):
         super().__init__("number_publisher")
-        self.number = 2
+        self.declare_parameter("published_number",1)
+        self.declare_parameter("publish_frequency",1.0)
+
+        self.number = self.get_parameter("published_number").value
+        self.publish_frequency = self.get_parameter("publish_frequency").value
         self.number_publisher = self.create_publisher(Int64, "number", 10)   
-        self.number_timer = self.create_timer(2, self.publish_number)
+        self.number_timer = self.create_timer(1/self.publish_frequency, self.publish_number)
         self.get_logger().info("Enviando o número " + str(self.number))
  
     def publish_number(self):
